9bd5e3ef0541381d1275b29b28d50862636164ce
João Josézinho <thepaperless@gmail.com>
2022-03-29 21:35:37 +0200

    adding shallow copies

---
+++ shallow_copy.py
@@ -0,0 +1,23 @@
+import copy
+
+animals = {
+	"lion": ["scary", "big", "cat"],
+	"elephant": ["big", "grey", "wrinkled"],
+	"teddy": ["cuddly", "stuffed"],
+}
+
+# perform a shallow copy:
+things = animals.copy()
+
+# perform a deep copy
+# things = copy.deepcopy(animals)
+
+print(id(things["teddy"]), things["teddy"])
+print(id(animals["teddy"]), animals["teddy"])
+
+print()
+
+things["teddy"].append("toy")
+
+print(things["teddy"])
+print(animals["teddy"])
